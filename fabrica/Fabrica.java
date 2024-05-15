8b7be2ca2f688f567c1354ecf205a6bc054ea0af
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-14 23:10:21 -0300

    feat: create fabrica

--- fabrica/Fabrica.java
+++ fabrica/Fabrica.java
@@ -5,7 +5,23 @@ import POO_Desafio.iphone.Iphone;
 public class Fabrica {
     public static void main(String[] args) {
         Iphone iphone = new Iphone();
+
+        System.out.println("Funções de Telefone no IPhone:-------------------------");
         iphone.atender();
+        iphone.ligar();
+        iphone.iniciarCorreioVoz();
+
+        System.out.println("\nFunções de Navegador no IPhone:-----------------------");
+        iphone.exibirPagina();
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+
+        System.out.println("\nFunções de Navegador no IPhone:-----------------------");
+        iphone.tocar();
+        iphone.pausar();
+        iphone.SelecionarMusica();
+        System.out.println("\n \n FIM \n");
+
     }
     
 }
